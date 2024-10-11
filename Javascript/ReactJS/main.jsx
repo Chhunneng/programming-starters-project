a39c1e513587da8e023fc12ebd9360cf91e62091
Utkarshgola <utkarsh37.82@gmail.com>
2024-10-11 12:45:31 +0530

    Update main.jsx

--- Javascript/ReactJS/main.jsx
+++ Javascript/ReactJS/main.jsx
@@ -1,10 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
